4e58c4e61f607ae3b8c7c88711fff0b52620dff6
Anya S <shipkov.alexey@gmail.com>
2022-08-06 20:37:06 +0300

    Comments taken into account

--- homework.py
+++ homework.py
@@ -1,4 +1,5 @@
-from http.client import OK
+from http import HTTPStatus
+from typing import Dict, List
 import os
 import time
 import requests
@@ -35,9 +36,10 @@ NOT_STATUS_KEY = 'Ошибка: отсутствует ключ "status".'
 NOT_HOMEWORK_STATUS = 'Ошибка: Незадокументированный статус домашней работы'
 NOT_ENDPOINT = f'Не удалось получить доступ к эндпоинту {ENDPOINT}.'
 NOT_JSON = 'Не удалось не удалось обработать json.'
+EMPTY_LIST = 'Список домашних работ пуст.'
 
 
-def init_logger() -> None:
+def init_logger() -> logging.Logger:
     """Создает логгер."""
     formatter = logging.Formatter('%(asctime)s, %(levelname)s, %(message)s')
     logger = logging.getLogger(__name__)
@@ -51,7 +53,7 @@ def init_logger() -> None:
 logger = init_logger()
 
 
-def send_message(bot, message) -> None:
+def send_message(bot: Bot, message: str) -> None:
     """Высылает сообщение через телеграм-бот."""
     try:
         bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
@@ -61,14 +63,14 @@ def send_message(bot, message) -> None:
         raise TelegramError(f'Произошла{error}')
 
 
-def get_api_answer(current_timestamp) -> dict:
+def get_api_answer(current_timestamp: int) -> Dict[str, list]:
     """Функция получает ответ от API."""
     timestamp = current_timestamp
     payload = {'from_date': timestamp}
 
     try:
         response = requests.get(ENDPOINT, headers=HEADERS, params=payload)
-        if response.status_code != OK:
+        if response.status_code != HTTPStatus.OK:
             logger.error(
                 f'{NOT_ENDPOINT}'
                 f'Ошибка: {response.status_code}')
@@ -86,35 +88,39 @@ def get_api_answer(current_timestamp) -> dict:
         raise JSONDecodeError(NOT_JSON)
 
 
-def check_response(response) -> list:
+def check_response(response: Dict[str, list]) -> List[str]:
     """Функция проверяет ответ от API на соответствие критериям."""
     if not isinstance(response, dict):
         logger.error(NOT_DICT)
         raise TypeError(NOT_DICT)
-    if not response['homeworks']:
+    try:
+        homeworks = response['homeworks']
+    except KeyError:
         logger.error(NOT_EXIST)
         raise KeyError(NOT_EXIST)
     if not isinstance(response['homeworks'], list):
         logger.error(NOT_LIST)
         raise TypeError(NOT_LIST)
     try:
-        homeworks = response.get('homeworks')
-    except requests.exceptions.RequestException:
-        logger.error(NOT_EXIST)
-        raise requests.exceptions.RequestException(NOT_EXIST)
+        homeworks[0]
+    except IndexError:
+        logger.error(EMPTY_LIST)
+        raise IndexError(EMPTY_LIST)
     return homeworks
 
 
-def parse_status(homework) -> str:
+def parse_status(homework: List[str]) -> str:
     """Функция получает статус домашней работы."""
-    if not homework['homework_name']:
+    try:
+        homework_name = homework['homework_name']
+    except KeyError:
         logger.error(NOT_HOMEWORK_NAME_KEY)
         raise KeyError(NOT_HOMEWORK_NAME_KEY)
-    homework_name = homework.get('homework_name')
-    if not homework['status']:
+    try:
+        homework_status = homework['status']
+    except KeyError:
         logger.error(NOT_STATUS_KEY)
         raise KeyError(NOT_STATUS_KEY)
-    homework_status = homework.get('status')
     if homework_status not in HOMEWORK_STATUSES:
         logger.error(NOT_HOMEWORK_STATUS)
         raise KeyError(NOT_HOMEWORK_STATUS)
@@ -122,13 +128,9 @@ def parse_status(homework) -> str:
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
-def check_tokens() -> None:
+def check_tokens() -> bool:
     """Проверка на наличие всех необходимых элементов внешнего окружения."""
-    token_list = ('PRACTICUM_TOKEN', 'TELEGRAM_TOKEN', 'TELEGRAM_CHAT_ID')
-
-    if any(
-            globals()[token] is None or globals()[token] == ''
-            for token in token_list):
+    if not all([PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID]):
         logger.critical('Отсутствует обязательный элемент окружения!')
         return False
     return True
@@ -140,7 +142,9 @@ def main() -> None:
     if not check_tokens():
         raise SystemExit
     bot = Bot(token=TELEGRAM_TOKEN)
+
     current_timestamp = int(time.time())
+    print(type(current_timestamp))
     while True:
         try:
             response = get_api_answer(current_timestamp)
